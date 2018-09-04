27540cfacad9efe5480d20755e6b4c0abc78ba87
Gittifer <38451140+Gittifer@users.noreply.github.com>
2018-09-03 19:10:00 -0500

    removed useless prints, formatting

--- draw.lua
+++ draw.lua
@@ -24,61 +24,61 @@ end
 
 function gridlay(div,size,shiftx,shifty)
   
-  local div = div           -- number of divisions, only multiples of 4 divide cleanly
-  local size = (128/div)    -- space between gridlines
-  local shiftx = shiftx
-  local shifty = shifty
+	local div = div           -- number of divisions, only multiples of 4 divide cleanly
+	local size = (128/div)    -- space between gridlines
+	local shiftx = shiftx
+	local shifty = shifty
   
-    screen.line_width(1)    -- vertical gridlay
-    for i = 1, div do     
-      screen.move(i * size + shiftx, 0)
-      screen.line_rel(0,64)
-      if i < div/2 then
-          screen.level(i)
+ 	screen.line_width(1)    -- vertical gridlay
+    	for i = 1, div do     
+      		screen.move(i * size + shiftx, 0)
+      		screen.line_rel(0,64)
+      	if i < div/2 then
+         	screen.level(i)
         else 
-          screen.level(div-i)
-      end
-     screen.stroke()
-    end
+          	screen.level(div-i)
+      	end
+     	screen.stroke()
+	end
     
-    screen.level(7)         -- vertical centerline
-    screen.line_width(1) 
-    screen.move(64,0)
-    screen.line_rel(0,64)
-    screen.stroke()
-    
-    screen.line_width(1)    -- horizontal gridlay
-    for i = 1, div/2 do     
-      screen.move(0, i*size + shifty)
-      screen.line_rel(128,0)
-      if i < div/4 then
-          screen.level(i*2)
-        else
-          screen.level(div-i*2)
-      end
-      screen.stroke()
-    end
+    	screen.level(7)         -- vertical centerline
+    	screen.line_width(1) 
+	screen.move(64,0)
+	screen.line_rel(0,64)
+	screen.stroke()
+
+    	screen.line_width(1)    -- horizontal gridlay
+    	for i = 1, div/2 do     
+      		screen.move(0, i*size + shifty)
+      		screen.line_rel(128,0)
+      		if i < div/4 then
+          		screen.level(i*2)
+        	else
+          		screen.level(div-i*2)
+      		end
+      		screen.stroke()
+    	end
       
-    screen.level(15)        -- horizontal centerline
-    screen.line_width(1)
-    screen.move(0,32)
-    screen.line_rel(128,0)
-    screen.stroke()
+    	screen.level(15)        -- horizontal centerline
+    	screen.line_width(1)
+    	screen.move(0,32)
+    	screen.line_rel(128,0)
+    	screen.stroke()
     
-    screen.level(0)
-      screen.move(0,31)
-      screen.line_rel(128,0)
-      screen.move(0,33)
-      screen.line_rel(128,0)
-      screen.move(63,0)
-      screen.line_rel(0,64)
-      screen.move(65,0)
-      screen.line_rel(0,64)
-      screen.stroke()
+    	screen.level(0)
+      	screen.move(0,31)
+      	screen.line_rel(128,0)
+      	screen.move(0,33)
+      	screen.line_rel(128,0)
+      	screen.move(63,0)
+      	screen.line_rel(0,64)
+      	screen.move(65,0)
+      	screen.line_rel(0,64)
+      	screen.stroke()
     
-    screen.level(10)        -- center square
-    screen.rect(62,30,3,3)
-    screen.fill()
+    	screen.level(10)        -- center square
+    	screen.rect(62,30,3,3)
+    	screen.fill()
     
 end
 
@@ -101,49 +101,46 @@ local base = base                                       -- base only divides cle
 local value = util.clamp(value, 0, base^2 * 2)          -- a little impractical over 8.
 screen.line_width(0.5)
 
-  if value / base <= 1 and value % base > 0 then        -- val = 1,2,3...(b-1)
-    for i = 0,(value % base - 1) do                    	-- row 1
-      screen.pixel(x + i * 2, y)
-    end
+	if value / base <= 1 and value % base > 0 then        -- val = 1,2,3...(b-1)
+  		for i = 0,(value % base - 1) do                    	-- row 1
+      			screen.pixel(x + i * 2, y)
+    	end
     
-  elseif value / base == 1 and value % base == 0 then   -- val = b
-    for i = 0, base - 1 do                              -- row 1
-      screen.pixel(x + i * 2, y)
-    end
+	elseif value / base == 1 and value % base == 0 then   -- val = b
+    		for i = 0, base - 1 do                              -- row 1
+      			screen.pixel(x + i * 2, y)
+    		end
     
-  elseif value / base >= 1 and value % base > 0 then    -- val = more than b, but not a multiple of b
-    for i = 0,(value % base - 1) do                     -- row 1
-      screen.pixel(x + i * 2, y)
-      -- print("i:"..i * 2 + x)
-    end
+  	elseif value / base >= 1 and value % base > 0 then    -- val = more than b, but not a multiple of b
+    		for i = 0,(value % base - 1) do                     -- row 1
+      			screen.pixel(x + i * 2, y)
+    		end
     
-    for i = 1, (value // base) do                       -- row 2
-      screen.pixel(x + i - 1, y + 1)                           
-    end
-  elseif value / base > 1 and value % base == 0 then    -- value = multiples of base
-    for i = 0, base - 1 do
-      screen.pixel(x + i * 2, y)
-      -- print("n:"..i * 2 + x)
-    end
+    		for i = 1, (value // base) do                       -- row 2
+      			screen.pixel(x + i - 1, y + 1)                           
+    		end
+  	elseif value / base > 1 and value % base == 0 then    -- value = multiples of base
+    		for i = 0, base - 1 do
+      			screen.pixel(x + i * 2, y)
+    		end
     
-    for i = 1, (value // base) - 1 do                   -- row 2
-      screen.pixel(x + i - 1, y + 1)                          
-    end
-  end
+    		for i = 1, (value // base) - 1 do                   -- row 2
+      			screen.pixel(x + i - 1, y + 1)                          
+    		end
+  	end
   
-  if value == 0 then                                    -- create a shadow outline if value is 0
-    screen.level(1)
-    for i = 0, base - 1 do
-      screen.pixel(x + i * 2, y)
-      -- print("n:"..i * 2 + x)
-    end
-    for i = 1, base * 2 - 1 do                  
-      screen.pixel(x + i - 1, y + 1)
-    end
+  	if value == 0 then                                    -- create a shadow outline if value is 0
+    		screen.level(1)
+    		for i = 0, base - 1 do
+      			screen.pixel(x + i * 2, y)
+    		end
+    		for i = 1, base * 2 - 1 do                  
+      			screen.pixel(x + i - 1, y + 1)
+    		end
 
-    screen.fill()
-    screen.level(15)
-  end
+    	screen.fill()
+    	screen.level(15)
+  	end
   
-  screen.fill()
+screen.fill()
 end
